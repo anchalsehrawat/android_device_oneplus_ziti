a54da7bc6a300d21419977717d3aa06360c5c4bb
pjgowtham <pjgowtham@gmail.com>
2023-12-26 19:02:51 +0530

    ziti: Initial rebrand for OnePlus Nord CE3 5G
    Change-Id: I1d7decc0d1085ede4e1625f0b293a1cb43a916ee

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonade.mk
+    $(LOCAL_DIR)/lineage_ziti.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonade-user \
-    lineage_lemonade-userdebug \
-    lineage_lemonade-eng
+    lineage_ziti-user \
+    lineage_ziti-userdebug \
+    lineage_ziti-eng
