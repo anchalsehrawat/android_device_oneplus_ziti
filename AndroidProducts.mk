e8983a3996477e154669ef4941e645648a90f6ac
anchalsehrawat <anchalsingh987@gmail.com>
2024-09-29 16:20:14 +0530

    ziti: AOSP bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ziti.mk
+    $(LOCAL_DIR)/aosp_ziti.mk
--- aosp_ziti.mk
+++ aosp_ziti.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ziti device
 $(call inherit-product, device/oneplus/ziti/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_ziti
+PRODUCT_NAME := aosp_ziti
 PRODUCT_DEVICE := ziti
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
