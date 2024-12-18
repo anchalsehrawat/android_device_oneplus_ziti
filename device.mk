3d4379b2b0e37fa91bd99294b24c0421a47fdf18
anchalsehrawat <anchalsingh987@gmail.com>
2024-12-18 11:36:40 +0530

    ziti: Inherit Oplus Camera

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# OplusCamera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
 # Camera
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
